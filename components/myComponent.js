70a48125e36af83586dffd3dc02557bbd34ff878
JesusDavidJaimesSerranoADSO <jdavidjaimess@gmail.com>
2023-03-22 08:46:52 -0500

    feat: :sparkles: avance del taller
    implementacion de los botones de agregacion de objetos, mostrar arrays en pantalla y eliminacion de objetos meduante boton determinado

--- components/myComponent.js
+++ components/myComponent.js
@@ -1,17 +1,29 @@
 export default{
     data(){
         return{
+            lista:false,
             arrays:null,
             nuevaTarea:``
         }
     },
     methods:{
         crearArray(){
-            this.arrays=[]
+            this.arrays=[],
+            this.lista=true
         },
-        crearObjeto(){
+        crearObjeto2(){
             this.arrays.push(this.nuevaTarea)
             this.nuevaTarea=``
+        },
+        crearObjeto1(){
+            this.arrays.unshift(this.nuevaTarea)
+            this.nuevaTarea=``
+        },
+        elArray(){
+            this.arrays=null
+        },
+        elObjeto(del){
+            this.arrays.splice(del,1)
         }
     }
 }
